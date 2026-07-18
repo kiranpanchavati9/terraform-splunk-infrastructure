27f73e34a17ffe3420eaba119824206e81a9a67d
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-18 13:01:50 +0400

    tags updated

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -5,30 +5,30 @@ output "security_group_id" {
 
 output "security_group_name" {
   description = "The name of the security group"
-  value       = aws_security_group.this_security_group.name
+  value       = aws_security_group.this.name
 }
 
 output "security_group_description" {
   description = "The description of the security group"
-  value       = aws_security_group.this_security_group.description
+  value       = aws_security_group.this.description
 }
 
 output "security_group_vpc_id" {
   description = "The VPC ID of the security group"
-  value       = aws_security_group.this_security_group.vpc_id
+  value       = aws_security_group.this.vpc_id
 }
 
 output "security_group_ingress_rules" {
   description = "The ingress rules of the security group"
-  value       = aws_security_group.this_security_group.ingress
+  value       = aws_security_group.this.ingress
 }
 
 output "security_group_egress_rules" {
   description = "The egress rules of the security group"
-  value       = aws_security_group.this_security_group.egress
+  value       = aws_security_group.this.egress
 }
 
 output "security_group_tags" {
   description = "The tags of the security group"
-  value       = aws_security_group.this_security_group.tags
+  value       = aws_security_group.this.tags
 }
