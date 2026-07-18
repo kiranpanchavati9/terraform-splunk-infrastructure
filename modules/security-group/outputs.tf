6450f46b78c1c4ed43041cd6f72e92bed64e6121
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-18 13:02:57 +0400

    this id updated

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,6 +1,6 @@
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = aws_security_group.this_security_group.id
+  value       = aws_security_group.this.id
 }
 
 output "security_group_name" {
