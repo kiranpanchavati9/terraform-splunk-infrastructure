3dd939415d2b6054ad8ebc201b4d5d8f00c1e356
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-18 12:58:24 +0400

    outputs updated

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,34 +1,34 @@
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.id
+  value       = aws_security_group.this_security_group.id
 }
 
 output "security_group_name" {
   description = "The name of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.name
+  value       = aws_security_group.this_security_group.name
 }
 
 output "security_group_description" {
   description = "The description of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.description
+  value       = aws_security_group.this_security_group.description
 }
 
 output "security_group_vpc_id" {
   description = "The VPC ID of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.vpc_id
+  value       = aws_security_group.this_security_group.vpc_id
 }
 
 output "security_group_ingress_rules" {
   description = "The ingress rules of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.ingress
+  value       = aws_security_group.this_security_group.ingress
 }
 
 output "security_group_egress_rules" {
   description = "The egress rules of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.egress
+  value       = aws_security_group.this_security_group.egress
 }
 
 output "security_group_tags" {
   description = "The tags of the security group"
-  value       = aws_security_group.allow_splunk_firewall_traffic_prod_dev.tags
+  value       = aws_security_group.this_security_group.tags
 }
