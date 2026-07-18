1f737620bf6a44479432fd40a24b90fe3cf1f6e3
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-18 12:48:35 +0400

    updated security module

---
+++ environments/prod/main.tf
@@ -0,0 +1,13 @@
+module "splunk_security_group" {
+    source = "../../modules/security-group"
+    
+    vpc_id = var.vpc_id
+    security_group_name = var.security_group_name
+    security_group_description = var.security_group_description
+    cidr_ipv4 = var.cidr_ipv4
+    ip_protocol = var.ip_protocol
+    management_port = var.management_port
+    receiving_port = var.receiving_port
+    web_interface_port = var.web_interface_port
+    api_port = var.api_port
+}
---
+++ environments/prod/terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_id = "vpc-0fcbf944165ec4597"
+cidr_ipv4 = "0.0.0.0/0"
+security_group_name = "splunk-prod-sg"
+security_group_description = "Splunk Production Security Group" 
