b27b59c8ec0f143c5c48a3d4f694378d0d289344
kiranpanchavati9 <kiranpanchavati9@gmail.com>
2026-07-18 19:52:53 +0400

    security group updated

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   iam_instance_profile   = var.iam_instance_profile
-  vpc_security_group_ids = [module.security-group.security_group_ids]
+  vpc_security_group_ids = [module.splunk_security_group.security_group_id]
   tags = merge(
     {
       Name = each.key
